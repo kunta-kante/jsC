887f7c826f0a0f073c200241db366fd4bbd7f81f
Rahman <rahmanraynkunta@gmail.com>
2020-12-08 12:54:07 +0100

    premier commit

---
+++ terminal.js
@@ -0,0 +1,275 @@
+  // Bienvenue à ce premier challenge.
+
+  // Pour que les resultats soient visible dans le navigateur, vous utiliserez
+  // la fonction 'console.log'
+  // ex: console.log('simplon');
+  // ex: console.log(nom de la variable);
+  // Lorque vous avez validé un exercice, mettez le en commentaire !!
+
+
+
+
+
+  //Déclarer deux variables avec comme valeur 4 et 5
+  //Créez une autre variable contenant l'addition des deux et l'afficher.
+  //done
+  //   const x = 4;
+  //   const y = 5;
+  //   const elements = x + y;
+  //   console.log(elements);
+
+
+
+  //Afficher la chaine de caractère suivante en majuscule
+  //   const team = 'avengers';
+  //   console.log(team.toUpperCase());
+
+
+
+  //   // afficher la première lettre de la chaine de caractère suivante
+  //   const team = 'avengers';
+  //   const firstLetter = team[0];
+  //   console.log(firstLetter);
+
+
+
+
+  //   //afficher la chaine de caractère suivante exepté la première lettre.
+  //   const team = 'avengers';
+  //   console.log(team.substring(1));
+
+
+
+
+
+  //Afficher les deux chaines suivantes dans une seule chaine de caractère (concaténation)
+  //   const firstName = 'Tony';
+  //   const lastName = 'Stark';
+  //   const fullName = firstName + ' ' + lastName;
+  //   console.log(fullName);
+
+
+
+  //Afficher la chaine de caractère suivante avec seulement la première lettre en majuscule.
+  //   let team = 'aveNgers';
+  //   //=> Avengers
+  // //   const firstLetter = team[0].toUpperCase();
+  // //   const restLetter = (team.substring(1)).toLowerCase();
+  // //   console.log(firstLetter + restLetter);
+
+
+  // methode splice 
+  //Les tableaux
+  //Déclarez un tableau avec 5 villes
+  //Afficher la ville à l'indice 3
+  //Ajouter une ville au tableau
+  //Supprimer la ville à l'indice 2
+  let villes = ["freetown", "makeni", "kono", "kenema", "BO"];
+  console.log(villes[3]);
+  villes.splice(villes.length, 0, "kabala");
+  console.log(villes);
+  villes.splice(2, 1);
+  console.log(villes);
+
+
+
+  //Les boucles
+  //
+  //Comme tu le sais sans doute, pour parcourir un tableau,
+  // il faut Boucler sur ce tableau. Parcours le tableau suivant afin de monter chaque
+  //éléments au carré et afficher le tableau.
+
+  //   const array1 = [2, 4, 8];
+  //   console.log(array1);
+  //   array1.forEach((element) => {
+  //       console.log(Math.pow(element, 2));
+  //   })
+
+
+  // Le tableau suivant indique les notes d'une promo lointaine !
+  // Calculer la moyenne de cette promo et afficher le resultat
+  //   const promo = [12, 13, 17, 3, 14, 18];
+  //   const reducer = (accumulator, currentValue) => accumulator / currentValue;
+  //   console.log(promo.reduce(reducer));
+
+
+
+  //les conditions
+
+  //Déclarez une variable "age" avec la valeur de votre choix.
+  //En utilisant les conditions 'if' et 'else' affichez :
+  //peut voter si 'age' est supérieur ou égal à 18
+  //ne peut pas voter si 'age' est inférieur à 18
+  const age = 16;
+  if (age >= 18) {
+      console.log("peut voter");
+  } else {
+      console.log("ne peut pas voter");
+  };
+
+
+
+
+
+
+  // Afficher la date d'aujourd'hui avec javascript.
+  // En utilisant les conditions 'if' et 'else' affichez :
+  // "bonjour" si il est en 5h du matin et 17h
+  // et "bonsoir" s'il est entre 17h et 5h du matin
+  const date = new Date();
+  const heure = date.getHours();
+  if (5 < heure, heure < 17) {
+      console.log("bonjour");
+  } else {
+      console.log("bonsoir");
+  };
+
+
+
+
+
+  //----------------- Fonctions -------------------
+
+  //Créez deux variables initialisées à 3 et 4.
+  //Créez une fonction qui retourne l'addition de ces deux valeurs.
+  //   const x = 3;
+  //   const y = 4;
+  //   const add = (num1, num2) => {
+  //       return num1 + num2
+  //   };
+  //   console.log(add(x, y));
+
+
+
+
+
+  //Créez une fonction qui prend en paramètre la valeur i. La fonction doit
+  //retourner la valeur i au carré et l'afficher.
+
+  //   const square = (number) => {
+  //       return number * 2
+  //   };
+  //   const i = 5;
+  //   console.log(square(i));
+
+
+
+  // En utilisant la methode 'map', mettre la chaine de
+  // caractère en majuscule.
+  //   const names = ["paul", "john", "ringo", "george"];
+  //   const namesMaj = names.map((Element) => Element.toUpperCase())
+  //   console.log(namesMaj);
+
+
+
+  //Créez un tableau nommé tab dont le premier
+  //élément est 2, le deuxième 3 et le troisième 4.
+  //Créez une fonction qui renvoit la somme des éléments du tableau
+  //   const tab = [2, 3, 4]
+  //   const reducer = (a, b) => a + b;
+  //   const somme = tab.reduce(reducer);
+  //   //   console.log(somme);
+  //   //Tri à bulle
+  //   //Classé les éléments du tableau suivant par ordre croissant.
+  //   //Affichez le tableau classé.
+  //   console.log(tab.sort());
+
+
+
+
+
+
+
+
+  //Créez une fonction qui prend un parametre 'r' et qui
+  //renvoie le périmetre d'un cercle.
+  // const périmetreCercle = (r) => {
+  //     return 2*r*3.14 
+  //   };
+  //   console.log(périmetreCercle(r));
+
+
+
+
+
+  //Créez une fonction qui met la premiere lettre d'un mot en majuscule.
+  //Le reste du mot doit être en minuscule.
+  //Le mot est "SimPloN"
+  mot = "SimPlon"
+  const capitalize = (word) => {
+      console.log(word[0].toUpperCase() + word.slice().toLowerCase());
+  };
+  capitalize(mot);
+
+
+
+
+  //Odd or Even
+  //Créez un fonction qui prend un nombre en parametre et qui renvoie :
+  //paire si le nombre est pair
+  //impaire si le nombre est impair
+  //Tips : jetez un coup d'oeil à l'opérateur '%' dans la doc
+  // const nombre = (element) => {
+  //   if ((element%2)===0){
+  //     console.log(`${element} est un nombre pair`)
+  //   }else {
+  //     console.log(`${element} est un nombre impair`)
+  //   }
+  // };
+  // nombre(6)
+
+
+
+
+
+
+
+  //Mot inversé
+  //créez une fonction qui prend en paramètre une chaine de caractère.
+  //Cette fonction renvoie la chaine de caractère inversée.
+  //   ex: reverseWord('simplon') ==> 'nolpmis'
+  //   Aide : join(), split(), reverse()
+
+  //   const reverseWord = (word) => {
+  //       console.log(word.reverse());
+  //   };
+  //   reverseWord('simplon')
+
+  //   function reverseString(str) {
+  //       console.log(str.split("").reverse().join(""));
+  //   }
+  //   reverseString("simplon");
+
+  //   const string = (str) => {
+  //       console.log(str.split("").reverse().join(""));
+  //   }
+  //   string("simplon");
+
+
+
+
+
+
+
+  //Reprendre la fonction qui permet de calculer la somme d'un tableau
+  //Le tableau est le suivant : 
+  const nombres = [3, 5, 6, 2];
+  const sum = nombres.reduce((a, b) => {
+      return a + b
+  })
+  console.log(sum)
+
+
+  //Créez une fonction qui renvoie la somme de chaque éléments du
+  //tableau si celui-ci est positif.
+  const array2 = [-2, 4, -5, 3, 6];
+  array2.map((i) => {
+      if (i < 0) {
+          console.log(array2.splice(i, 0))
+      }
+  });
+
+  const addPositiveNumber = array2.reduce((a, b) => {
+      return a + b
+  });
+  console.log(addPositiveNumber);
